ccbecb5a505fafa4b0aa6a20a2784e423118e3bb
Jose Gamboa <jgamboa@kinedu.com>
2019-08-24 13:35:31 -0500

    Movement

--- duckies/Assets/Scripts/Juego.cs
+++ duckies/Assets/Scripts/Juego.cs
@@ -6,23 +6,32 @@ public class Juego : MonoBehaviour
 {
     static public float floatingPoint = 2f;
     static public float jumpingForce = 7f;
+    static public float speed = 15f;
+    static public float speedG = 5f;
 
     public GameObject Prefab;
     public Transform pos;
+    public GameObject waterPlane;
+
     GameObject player1, player2;
 
     // Use this for initialization
+    void Awake()
+    {
+        waterPlane = Instantiate(waterPlane, new Vector3(0f, -95f, 0f), Quaternion.identity);
+    }
+
     void Start()
     {
-        /*
         player1 = Instantiate(Prefab, pos.position, Quaternion.identity);
         player1.GetComponent<mov>().horizontal="Horizontal";
         player1.GetComponent<mov>().vertical = "Vertical";
+        player1.GetComponent<Float>().WaterLevel = waterPlane;
 
         player2 = Instantiate(Prefab, pos.position, Quaternion.identity);
         player2.GetComponent<mov>().horizontal="Horizontal2";
         player2.GetComponent<mov>().vertical="Vertical2";
-        */      
+        player2.GetComponent<Float>().WaterLevel = waterPlane;
     }
 
     // Update is called once per frame
--- duckies/Assets/Scripts/mov.cs
+++ duckies/Assets/Scripts/mov.cs
@@ -4,9 +4,15 @@ using UnityEngine;
 
 public class mov : MonoBehaviour
 {
-    public float speed;
-    public float speedG;
+    float speed;
+    float speedG;
     public string horizontal, vertical;
+
+    private void Start()
+    {
+        speed = Juego.speed;
+        speedG = Juego.speedG;
+    }
 
     // Update is called once per frame
     void Update()
@@ -18,10 +24,13 @@ public class mov : MonoBehaviour
         Vector3 velo = GetComponent<Rigidbody>().velocity;
 
         if (Input.GetKey("up") || Input.GetKey("w") || Input.GetKey("s") || Input.GetKey("down"))
-            transform.Translate(new Vector3(axisY, velo.y, velo.z)*Time.deltaTime);
-        
-        
-        if(Input.GetKey("left") || Input.GetKey("right") || Input.GetKey("a") || Input.GetKey("d"))
+        {
+            transform.Translate(new Vector3(axisY, velo.y, velo.z) * Time.deltaTime);
+        } else {
+            GetComponent<Rigidbody>().velocity = new Vector3(0f, velo.y, 0f);
+        }
+
+        if (Input.GetKey("left") || Input.GetKey("right") || Input.GetKey("a") || Input.GetKey("d"))
             transform.Rotate(new Vector3(0, axisX, 0));
         
     }
