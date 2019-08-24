9619086b6791a677ecf59ff2084ee436b93c25dd
Jose Gamboa <jgamboa@kinedu.com>
2019-08-24 13:05:06 -0500

    Good floating point

---
+++ duckies/Assets/Scripts/Float.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Float : MonoBehaviour
+{
+    public GameObject WaterLevel;
+
+    private float waterlevel;
+    private float floatHeight;
+
+    private void Start()
+    {
+        waterlevel = WaterLevel.GetComponent<Transform>().position.y;
+        floatHeight = waterlevel + Juego.floatingPoint;
+    }
+
+    public void Update()
+    {
+        Vector3 velo = GetComponent<Rigidbody>().velocity;
+        
+        if (transform.position.y < floatHeight)
+            GetComponent<Rigidbody>().velocity = new Vector3(velo.x, Juego.jumpingForce, velo.z);
+    }
+}
---
+++ duckies/Assets/Scripts/Juego.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Juego : MonoBehaviour
+{
+    static public float floatingPoint = 2f;
+    static public float jumpingForce = 7f;
+
+    public GameObject Prefab;
+    public Transform pos;
+    GameObject player1, player2;
+
+    // Use this for initialization
+    void Start()
+    {
+        /*
+        player1 = Instantiate(Prefab, pos.position, Quaternion.identity);
+        player1.GetComponent<mov>().horizontal="Horizontal";
+        player1.GetComponent<mov>().vertical = "Vertical";
+
+        player2 = Instantiate(Prefab, pos.position, Quaternion.identity);
+        player2.GetComponent<mov>().horizontal="Horizontal2";
+        player2.GetComponent<mov>().vertical="Vertical2";
+        */      
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+}
